7319e1fa23b04f67273b85b4b8d09c8914fabe41
Arman <Arman@DESKTOP-20C59F2>
2026-09-15 17:17:33 -0300

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FitArmLog
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FitArmLog")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FitArmLog.Pages")]
